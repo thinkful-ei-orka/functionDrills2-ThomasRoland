f2534e5a8927496534f0f6d3bb7ce411b66d08a4
Thomas Davis <ThomasDavisA@gmail.com>
2020-04-20 22:24:22 -0700

    days in a month function added

--- main.js
+++ main.js
@@ -20,41 +20,78 @@ function beyond(num) {
 }
 
 function decode(word) {
-    // input word
-    // index =
-    // switch word[0]
-    // a = 2
-    // b=3
-    // c = 4
-    // d = 5
-    //default === space
-     // return word[index]
-    let index = "";
+
+    let index = '';
     switch (word[0]) {
-        case 'a':
-            index = 1;
-            break;
-        case 'b':
-            index = 2;
-            break;
-        case 'c':
-            index = 3;
-            break;
-        case  'd':
-            index = 4;
-            break;
-        default:
-            return ' ';
+    case 'a':
+        index = 1;
+        break;
+    case 'b':
+        index = 2;
+        break;
+    case 'c':
+        index = 3;
+        break;
+    case  'd':
+        index = 4;
+        break;
+    default:
+        return ' ';
     }
     return word[index];
 }
-let words = 'craft block argon meter bells brown croon droop';
+
+//How many days in a month
+//taking in month, leap year
+//switch (month)
+//return days in month
+//(jan mar may >july aug< oct dec)
+//return month has 31 days
+//(feb(not really) apr jun sep nov)
+//return month has 30 days
+//if feb, check leap year
+//default is 'needs a month'
+
+function daysInAMonth(month, leapYear) {
+    switch(month) {
+    case 'January':
+    case 'March':
+    case 'May':
+    case 'July':
+    case 'August':
+    case 'October':
+    case 'December':
+        return `${month} has 31 days`;
+    case 'April':
+    case 'June':
+    case 'September':
+    case 'November':
+        return `${month} has 30 days`;
+    case 'February':
+        if(leapYear) {
+            return `${month} has 29 days`;
+        } else {
+            return `${month} has 28 days`;
+        }
+    default:
+        throw 'Must provide a valid month.';
+    }
+}
+
+
+
 //Test functions
 console.log(jediName('Kevin', 'Bacon'));
 console.log(beyond(Infinity));
+
+let words = 'craft block argon meter bells brown croon droop';
+let decodedWord = '';
 let array = words.split(" ");
 for (let i =0; i < 8; i++) {
-    console.log(decode(array[i]));
+    decodedWord += decode(array[i]);
 }
-
+console.log(decodedWord);
+console.log(daysInAMonth('January'));
+console.log(daysInAMonth('February', true));
+console.log(daysInAMonth('haha'));
 
